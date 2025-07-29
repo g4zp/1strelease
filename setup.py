bae2576b02a4e4d7512be0e5dd3364210d94b4b3
Gustavo Azpurua <geazpuruac@gmail.com>
2025-07-29 15:21:54 +0200

    changes

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='1strelease',
+    version='0.1',
+    packages=find_packages(),
+    author='G4zp',
+    author_email='geazpuruac@gmail.com',
+    url='https://github.com/g4zp/1strelease',
+)
